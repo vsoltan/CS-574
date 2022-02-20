3fd5f8a34757ba3f9c1491200ab78a8d7eefa095
Valeriy Soltan <vsoltan@umass.edu>
2022-02-20 15:29:00 -0500

    added weight initializations, run resulted in 11, 15.5% test error for each prediction strategy respectively

--- hw2/model.py
+++ hw2/model.py
@@ -9,17 +9,33 @@ class Conv2dConfig():
         self.N, self.C_in, self.H, self.W = input_size 
         self.kernel_size, self.C_out = kernel_size, out_channels
         self.stride, self.padding = stride, padding 
+
         self.H_out = conv_out_size(self.H, self.kernel_size[0], \
             self.stride, self.padding)
+
         self.W_out = conv_out_size(self.W, self.kernel_size[1], \
             self.stride, self.padding)
+
         self.groups, self.bias = groups, bias 
 
     ''' created layer takes input with size (N, C_in, H, W) and produces output with size 
         (N, C_out, H_out, W_out)'''
-    def createLayer(self):
-        return nn.Conv2d(self.C_in, self.C_out, self.kernel_size, \
-            self.stride, self.padding, 1, self.groups, self.bias) 
+    def createLayer(self, init_scheme):
+        layer = nn.Conv2d(self.C_in, self.C_out, self.kernel_size, \
+            self.stride, self.padding, 1, self.groups, self.bias)
+
+        # initialize weights and biases
+        if layer.bias is not None: 
+            nn.init.constant_(layer.bias.data, 0)
+
+        if init_scheme == 'kaiming':
+            nn.init.kaiming_uniform_(layer.weight, 0.01)
+        elif init_scheme == 'xavier':
+            nn.init.xavier_uniform_(layer.weight)
+        else:
+            print("undefined weight initialization scheme, using default.")
+
+        return layer  
 
 class CNN(nn.Module):
     def __init__(self, num_classes):
@@ -29,31 +45,39 @@ class CNN(nn.Module):
         DEFINE YOUR NETWORK HERE
         
         """
+        # data set parameters 
+        num_views, in_channels, in_height, in_width = 12, 1, 112, 112
 
-        self.leaky_relu = nn.LeakyReLU(0.01) 
-        self.max_pool = nn.MaxPool2d((2, 2), 2)
+        self.leaky_relu = nn.LeakyReLU(negative_slope=0.01) 
+        self.max_pool = nn.MaxPool2d(kernel_size=(2, 2), stride=2)
 
-        conf1 = Conv2dConfig((12, 1, 112, 112), 8, (7, 7), 1, 0) 
-        self.conv_1 = conf1.createLayer() 
+        conf1 = Conv2dConfig(input_size=(num_views, in_channels, in_height, in_width), out_channels=8, \
+            kernel_size=(7, 7), stride=1, padding=0) 
+        self.conv_1 = conf1.createLayer(init_scheme='kaiming') 
         self.layer_norm_1 = nn.LayerNorm((conf1.C_out, conf1.H_out, conf1.W_out))
         
-        conf2 = Conv2dConfig((12, conf1.C_out, int(conf1.H_out / 2), int(conf1.W_out / 2)), 8, (7, 7), 2, 0, 8)
-        self.conv_2 = conf2.createLayer()
+        conf2 = Conv2dConfig(input_size=(num_views, conf1.C_out, int(conf1.H_out / 2), int(conf1.W_out / 2)), \
+            out_channels=8, kernel_size=(7, 7), stride=2, padding=0, groups=8)
+        self.conv_2 = conf2.createLayer(init_scheme='kaiming')
         self.layer_norm_2 = nn.LayerNorm((conf2.C_out, conf2.H_out, conf2.W_out))
 
-        conf3 = Conv2dConfig((12, conf2.C_out, int(conf2.H_out / 2), int(conf2.W_out / 2)), 16, (1, 1), 1, 0)
-        self.conv_ptwise_1 = conf3.createLayer()
+        conf3 = Conv2dConfig(input_size=(num_views, conf2.C_out, int(conf2.H_out / 2), int(conf2.W_out / 2)), \
+            out_channels=16, kernel_size=(1, 1), stride=1, padding=0, groups=1, bias=True)
+        self.conv_ptwise_1 = conf3.createLayer(init_scheme='xavier')
 
-        conf4 = Conv2dConfig((12, conf3.C_out, conf3.H_out, conf3.W_out), 16, (7, 7), 1, 0, 16)
-        self.conv_dwise = conf4.createLayer() 
+        conf4 = Conv2dConfig(input_size=(num_views, conf3.C_out, conf3.H_out, conf3.W_out), out_channels=16, \
+            kernel_size=(7, 7), stride=1, padding=0, groups=16)
+        self.conv_dwise = conf4.createLayer(init_scheme='kaiming') 
         self.layer_norm_3 = nn.LayerNorm((conf4.C_out, conf4.H_out, conf4.W_out))
 
-        conf5 = Conv2dConfig((12, conf4.C_out, conf4.H_out, conf4.W_out), 32, (1, 1), 1, 0)
-        self.conv_ptwise_2 = conf5.createLayer()
+        conf5 = Conv2dConfig(input_size=(num_views, conf4.C_out, conf4.H_out, conf4.W_out), out_channels=32, \
+            kernel_size=(1, 1), stride=1, padding=0, groups=1, bias=True)
+        self.conv_ptwise_2 = conf5.createLayer(init_scheme='xavier')
 
-        conf6 = Conv2dConfig((12, conf5.C_out, int(conf5.H_out / 2), int(conf5.W_out / 2)), \
-            num_classes, (int(conf5.H_out / 2), int(conf5.W_out / 2)), 1, 0, 1, True)
-        self.fc = conf6.createLayer()
+        conf6 = Conv2dConfig(input_size=(num_views, conf5.C_out, int(conf5.H_out / 2), int(conf5.W_out / 2)), \
+            out_channels=num_classes, kernel_size=(int(conf5.H_out / 2), int(conf5.W_out / 2)), stride=1, padding=0, \
+            groups=1, bias=True)
+        self.fc = conf6.createLayer(init_scheme='xavier')
         
     def forward(self, x):
         """
